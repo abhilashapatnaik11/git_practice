5b3cc5c0cd6bacd1ee8560ebfd08068c883e2037
Abhilasha_P <abhilasha.p@iserveu.co.in>
2024-07-23 22:22:58 +0530

    go variable

--- main.go
+++ main.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hey, bhalo")
+	var a,b,c int
+	a=10
+	b=10
+	c= a+b
+	fmt.Println("Sum of a and b is",c)
+
+	m:=102
+	n:=30
+	x:=m+n
+	fmt.Println("sum of m and n",x)
+
+	
 }
